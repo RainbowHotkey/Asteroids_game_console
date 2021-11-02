8eccd8f0e734aed189d6dc833b69b3523914f354
RainbowHotkey <hotkeysplayer@gmail.com>
2021-11-03 01:01:35 +0300

    Добавьте файлы проекта.

---
+++ Asteroids/Asteroids.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+#include "Game_engine/olcConsoleGameEngine.h"
+
+class Hotkeys_Asteroids : public olcConsoleGameEngine
+{
+public:
+    Hotkeys_Asteroids()
+    {
+        m_sAppName = L"Asteroids";
+    }
+    
+private:
+
+protected:
+    // called by olcConsoleGameEngine
+    virtual bool OnUserCreate()
+    {
+        return true;
+    }
+
+    // called by olcConsoleGameEngine
+    virtual bool OnUserUpdate(float fElapsedTime)
+    {
+        // clear screen
+        Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, 0);
+        return true;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
